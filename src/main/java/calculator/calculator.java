828ccd1d8cdb9e7ca20b600e2c2abb787cd6c0d2
Naidu2658 <kodanda.brahma@iiitb.ac.in>
2022-04-13 23:16:25 +0530

    created project and implemented calculator functionlaities along with test cases

--- src/main/java/calculator/calculator.java
+++ src/main/java/calculator/calculator.java
@@ -1,6 +1,5 @@
 package calculator;
 
-import java.util.Arrays;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -8,39 +7,63 @@ public class calculator {
   public static void  main(String args[])
   {
      calculator c= new calculator();
-      System.out.println("enter option 1 to add and 2 to sub");
+      System.out.println("enter 1 to find square root of a number");
+      System.out.println("enter 2 to find factorial of a number");
+      System.out.println("enter 3 to find natural logarithm of a number");
+      System.out.println("enter 4 to power of x to a");
       Scanner sc=new Scanner(System.in);
-      int num1, num2;
+      int choice;
+      int num1, num2=0;
+      try
+      {
+          choice=sc.nextInt();
+      }
+      catch (InputMismatchException e)
+      {
+          System.out.println(e);
+          return ;
+      }
       do{
-          int choice=0;
-          System.out.println("enter 1 or 2");
-          try {
-              choice= sc.nextInt();
-          }
-          catch(InputMismatchException e)
+          if(choice==1 || choice==2 || choice==3)
           {
-              System.out.println(e);
+              System.out.println("enter a number");
+
+              try
+              {
+                  num1=sc.nextInt();
+              }
+              catch(InputMismatchException e)
+              {
+                  System.out.println(e);
+                  return;
+              }
           }
-          try
-          {
-              System.out.println("enter first num");
-              num1=sc.nextInt();
-              System.out.println("enter second num");
-              num2=sc.nextInt();
-          }
-          catch(InputMismatchException e)
-          {
-              System.out.println(e);
-              return ;
+          else {
+              try {
+
+                  System.out.println("enter x");
+                  num1 = sc.nextInt();
+                  System.out.println("enter b");
+                  num2 = sc.nextInt();
+              } catch (InputMismatchException e) {
+                  System.out.println(e);
+                  return;
+              }
           }
           switch (choice)
           {
               case 1:
-                  System.out.println("sum is :" + c.add(num1, num2));
+                  System.out.println("square root is :" + c.square_root(num1));
                   break;
               case 2:
-                      System.out.println("dif is :" + c.sub(num1, num2));
+                      System.out.println("factorial is :" + c.factorial(num1));
                       break;
+              case 3:
+                  System.out.println("natural logarithm is :" + c.natural_logarithm(num1));
+                  break;
+              case 4:
+                  System.out.println("exponential is :" + c.exponential(num1, num2));
+                  break;
               default:
                   System.out.println("exited");
                   break;
@@ -49,12 +72,27 @@ public class calculator {
       }while(true);
   }
 
- public  int add(int num1, int num2)
+ public double square_root(int num)
+ {
+     return Math.sqrt(num);
+ }
+  public int factorial(int num)
   {
-      return num1+num2;
+      int i;
+      int result=1;
+      for(i=1;i<=num;i++)
+      {
+          result=result*i;
+      }
+      return result;
   }
-  public int sub(int num1,int num2)
+
+  public double natural_logarithm(int num)
   {
-      return num1-num2;
+      return Math.log(num);
+  }
+  public double exponential(int num1,int num2)
+  {
+      return Math.pow(num1, num2);
   }
 }
--- src/test/java/calculator/calculatorTest.java
+++ src/test/java/calculator/calculatorTest.java
@@ -1,24 +1,51 @@
 package calculator;
 
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.*;
 public class calculatorTest {
     calculator c=new calculator();
-
+    private static final double DELTA = 1e-15;
     @Test
-    public void addingfortrueaddition()
+    public void checking_for_true_square_root()
     {
-        assertEquals("adding two numbers" ,  8, c.add(4,4));
+        assertEquals("square root of a  number" ,  2, c.square_root(4), DELTA);
     }
     @Test
-    public void addingfortruesub()
+    public void checking_for_true_factorial()
     {
-        assertEquals("adding two numbers" ,  0, c.sub(5,5));
+        assertEquals("factorial of a  number" ,  24, c.factorial(4));
+    }
+    @Test
+    public void checking_for_true_natural_log()
+    {
+        assertEquals("natural log of a  number" ,  1.3862943611198906, c.natural_logarithm(4),DELTA);
+    }
+    @Test
+    public void checking_for_true_exponential()
+    {
+        assertEquals("exponential of x to a" ,  16.0, c.exponential(4, 2), DELTA);
     }
 
     @Test
-    public void addingforfasleaddition()
+    public void checking_for_false_square_root()
     {
-        assertNotEquals("adding two numbers" ,  8, c.add(4,3));
+        assertNotEquals("square root of a  number" ,  23, c.square_root(4), DELTA);
+    }
+    @Test
+    public void checking_for_false_factorial()
+    {
+        assertNotEquals("factorial of a  number" ,  242, c.factorial(4));
+    }
+    @Test
+    public void checking_for_false_natural_log()
+    {
+        assertNotEquals("natural log of a  number" ,  12.3862943611198906, c.natural_logarithm(4),DELTA);
+    }
+    @Test
+    public void checking_for_false_exponential()
+    {
+        assertNotEquals("exponential of x to a" ,  162.0, c.exponential(4, 2), DELTA);
     }
 }
